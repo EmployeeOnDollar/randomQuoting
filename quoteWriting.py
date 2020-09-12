db25049b17878de547bf4773087d2243e3ad626e
EmployeeOnDollar <59126795+EmployeeOnDollar@users.noreply.github.com>
2020-09-12 23:33:06 +0200

    Delete quoteWriting.py
    was'nt necessary

--- quoteWriting.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-# get input from user to create a new data frame
-def get_quote():
-    quote = input('Enter a Quote: ')
-    quoter = input('From: ')
-    new_dt = pd.DataFrame({'Quote': pd.Series(quote), 'Quoter': pd.Series(quoter)})
-    return new_dt
