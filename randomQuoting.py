5d6418747e8f4415355f33113e96023132a2db71
EmployeeOnDollar <59126795+EmployeeOnDollar@users.noreply.github.com>
2020-09-26 19:02:14 +0200

    Add files via upload
    Improved the code by adding infinite while loop for asking new input until saying no and undefined.

--- randomQuoting.py
+++ randomQuoting.py
@@ -3,7 +3,7 @@ import numpy as np
 import os.path
 
 
-# get input from user to create a new data frame
+# gets quote input from user to create a new data frame
 def get_quote():
     quote = input('Enter a Quote: ')
     owner = input('From: ')
@@ -11,7 +11,7 @@ def get_quote():
     return newdt
 
 
-# asks question to user
+# takes add or don't add input from user. turns answer into lower case
 def want2add(question):
     ans = input(question).lower()
     if ans == 'y' or ans == 'n':
@@ -20,23 +20,28 @@ def want2add(question):
         return want2add(question)
 
 
-if os.path.isfile('All_Quotes.csv'):
-    answer = want2add('Do you want to add new quote (y/n): ')
-    if answer == 'y':
-        print('File Exists. This entry will be added to file.')
-        dt = pd.read_csv('All_Quotes.csv', sep=';', index_col=0)
-        new_dt = get_quote()
-        dt = dt.append(new_dt)
-        dt.to_csv('All_Quotes.csv', sep=';')
-    elif answer == 'n':
-        dt = pd.read_csv('All_Quotes.csv', sep=';', index_col=0)
+while True:
+    if os.path.isfile('All_Quotes.csv'):
+        answer = want2add('Do you want to add new quote (y/n): ')
+        if answer == 'y':
+            print('File Exists. This entry will be added to file.')
+            dt = pd.read_csv('All_Quotes.csv', sep=';', index_col=0)
+            new_dt = get_quote()
+            dt = dt.append(new_dt)
+            dt.to_csv('All_Quotes.csv', sep=';')
+            dt = pd.read_csv('All_Quotes.csv', sep=';', index_col=0)
+        elif answer == 'n':
+            dt = pd.read_csv('All_Quotes.csv', sep=';', index_col=0)
+            break
+        else:
+            print('Wrong input type.' + '\nChoose "y" or "n"')
+            dt = pd.DataFrame([0])
+            break
     else:
-        print('Wrong input type.' + '\nChoose "y" or "n"')
-        dt = pd.DataFrame([0])
-else:
-    print("File doesn't exist. It'll be created.")
-    dt = get_quote()
-    dt.to_csv('All_Quotes.csv', sep=';')
+        print("File doesn't exist. It'll be created.")
+        dt = get_quote()
+        dt.to_csv('All_Quotes.csv', sep=';')
+        break
 
 
 # selects a quote from 'data' data frame
@@ -46,6 +51,7 @@ def select_quote(data):
     return sel_quote
 
 
+dt = pd.read_csv('All_Quotes.csv', sep=';', index_col=0)
 chosen = select_quote(dt)
 chosen_quote = chosen[0]
 chosen_owner = chosen[1]
